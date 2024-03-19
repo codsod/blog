3d33e83935f8891cefc26ec1bcb5c4008e972fa1
codsod <3140323339@qq.com>
2024-03-19 22:06:44 +0800

    master

--- 每周作业/as4/二叉树深度.py
+++ 每周作业/as4/二叉树深度.py
@@ -1,27 +1,30 @@
 class Node:
+
     def __init__(self):
-        self.left=None
-        self.right=None
+        self.left = None
+        self.right = None
+
 
 def tree_depth(node):
-    if node==None:
+    if node == None:
         return 0
-    left=tree_depth(node.left)
-    right=tree_depth(node.right)
-    return max(left,right)+1
+    left = tree_depth(node.left)
+    right = tree_depth(node.right)
+    return max(left, right) + 1
 
-n=int(input())
-nodes=[Node() for _ in range(n)]
-parent=[-1]*n
+
+n = int(input())
+nodes = [Node() for _ in range(n)]
+parent = [-1] * n
 for i in range(n):
-    leftindex,rightindex=map(int,input().split())
-    if leftindex!=-1:
-        nodes[i].left=nodes[leftindex-1]
-        parent[leftindex-1]=i
-    if rightindex!=-1:
-        nodes[i].right=nodes[rightindex-1]
-        parent[rightindex-1]=i
+    leftindex, rightindex = map(int, input().split())
+    if leftindex != -1:
+        nodes[i].left = nodes[leftindex - 1]
+        parent[leftindex - 1] = i
+    if rightindex != -1:
+        nodes[i].right = nodes[rightindex - 1]
+        parent[rightindex - 1] = i
 
-rootindex=parent.index(-1)
-root=nodes[rootindex]
+rootindex = parent.index(-1)
+root = nodes[rootindex]
 print(tree_depth(root))
--- 每周作业/as4/合法出栈队列.py
+++ 每周作业/as4/合法出栈队列.py
@@ -1,29 +1,29 @@
-def is_stackout_reasonable(init,post):
-    if len(init)!=len(post):
+def is_stackout_reasonable(init, post):
+    if len(init) != len(post):
         return 'NO'
-    postindex=0
-    stack=[]
+    postindex = 0
+    stack = []
     for i in init:
         stack.append(i)
-        if i==post[postindex]:
-            postindex+=1
+        if i == post[postindex]:
+            postindex += 1
             stack.pop()
-        while stack and stack[-1]==post[postindex]:
+        while stack and stack[-1] == post[postindex]:
             stack.pop()
-            postindex+=1
+            postindex += 1
     while stack:
-        if stack[-1]==post[postindex]:
+        if stack[-1] == post[postindex]:
             stack.pop()
-            postindex+=1
+            postindex += 1
         else:
             return 'NO'
     return 'YES'
 
-init=input()
+
+init = input()
 while True:
     try:
-        post=input()
-        print(is_stackout_reasonable(init,post))
+        post = input()
+        print(is_stackout_reasonable(init, post))
     except EOFError:
         break
-    
