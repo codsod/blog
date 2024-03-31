4412a4ae2f36cb7e7bf426485df09a6b3dd075ff
codsod <3140323339@qq.com>
2024-03-31 23:21:24 +0800

    每日选做的整理

---
+++ 每日选做/并查集/发现它，抓住它.py
@@ -0,0 +1,40 @@
+class UnionFind:
+    def __init__(self,n):
+        self.parent=list(range(n))
+        self.rank=[0]*n
+    def find(self,x):
+        if self.parent[x]!=x:
+            self.parent[x]=self.find(self.parent[x])
+        return self.parent[x]
+    def union(self,x,y):
+        x,y=self.find(x),self.find(y)
+        if x==y:
+            return
+        if self.rank[x]<self.rank[y]:
+            self.parent[x]=y
+        elif self.rank[x]>self.rank[y]:
+            self.parent[y]=x
+        else:
+            self.parent[y]=x
+            self.rank[x]+=1
+def solve():
+    n,m=map(int,input().split())
+    uf=UnionFind(2*n)
+    for _ in range(m):
+        oper,a,b=input().split()
+        a,b=int(a)-1,int(b)-1
+        if oper=='D':
+            uf.union(a,b+n)
+            uf.union(a+n,b)
+        else:
+            if uf.find(a)==uf.find(b) :
+                print("In the same gang.")
+            elif uf.find(a)==uf.find(b+n) or uf.find(a+n)==uf.find(b):
+                print("In different gangs.")
+            else:
+                print("Not sure yet.")
+
+
+T = int(input())
+for _ in range(T):
+    solve()
